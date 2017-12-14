37b69c23f08cd4a8059ff59b8e6915976bf98f1d
Nikita <MoofMonkey@users.noreply.github.com>
2017-12-14 07:42:42 +0300

    Revert

--- scripts/AutoDeny/body.js
+++ scripts/AutoDeny/body.js
@@ -15,18 +15,16 @@ function AutoDenyOnInterval() {
 			Rot_Damage = Abilities.GetLevelSpecialValueFor(Rot, "rot_damage", Abilities.GetLevel(Rot)),
 			SoulRing_Damage = Abilities.GetSpecialValueFor(SoulRing, "health_sacrifice")
 		if(Entities.GetHealth(MyEnt) <= Rot_Damage + SoulRing_Damage) {
-			if(Game.GetBuffsNames(MyEnt).indexOf("modifier_pudge_rot") === -1) {
-				Game.ToggleAbil(MyEnt, Rot, false)
-				return
-			}
 			if(SoulRing && Abilities.GetCooldownTimeRemaining(SoulRing) === 0)
 				Game.CastNoTarget(MyEnt, SoulRing, false)
+			if(Game.GetBuffsNames(MyEnt).indexOf("modifier_pudge_rot") === -1)
+				Game.ToggleAbil(MyEnt, Rot, false)
 			
 			return
 		}
 	}
 	
-	var item = Game.GetAbilityByName(MyEnt, BSName)
+	var item = Game.GetAbilityByName(MyEnt, "item_bloodstone")
 	if(!item)
 		return
 	Game.CastPosition(MyEnt, item, Entities.GetAbsOrigin(MyEnt), false)
